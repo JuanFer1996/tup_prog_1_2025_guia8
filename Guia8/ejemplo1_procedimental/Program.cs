760d6268b40905cf4bb494f9469d4eb448d6e79e
JuanFer1996 <juan_andres_fer@hotmail.com>
2025-05-22 16:22:52 -0300

    ejem1 procedimental

---
+++ Guia8/ejemplo1_procedimental/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ejemplo1_procedimental
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double prom=0;
+            int valor, cantidad, valorMin=0, valorMax=0, contador = 0, acumulador = 0;
+            Console.WriteLine("ingrse cantidad de ingresos");
+            cantidad = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < cantidad; i++)
+            {
+                Console.WriteLine("ingrese valor");
+                valor = Convert.ToInt32(Console.ReadLine());
+                acumulador += valor;
+                if (valor > valorMax || contador==0)
+                {
+                    valorMax = valor;
+                }
+                if (valor < valorMin||contador==0)
+                {
+                    valorMin = valor;
+                }
+                contador += 1;
+
+            }
+            if (acumulador > 0)
+            {
+                prom = acumulador / cantidad;
+            }
+            Console.WriteLine($"valor mañximo {valorMax} valor minimo {valorMin} promedio {prom}");
+        }
+    }
+}
